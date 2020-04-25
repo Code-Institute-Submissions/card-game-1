f2af97c12489ad82737325fc4f4453ed138d6c6c
Fatima Aminu <teemamin@yahoo.com>
2020-04-25 12:19:18 +0000

    card back image added, flip functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,70 @@
- let cardGameContainer = document.getElementById("cardContainer");
- let cardSizeOne = 134;
- let cardSizeTwo = 100;
- let cardMargin = 30;
- function getRandomArbitrary(min, max) {
+let cardGameContainer = document.getElementById("cardContainer");
+let cardSizeOne = 134;
+let cardMargin = 30;
+let gamerow = 4;
+let gamecol = 4;
+let firstcard = null;
+let secondcard = null;
+ /*function getRandomArbitrary(min, max) {
   return Math.round(Math.random() * (max - min) + min);
-}
- 
- function arrangeCrds(vertical,horizontal){
-     for(let i = 0; i < vertical; i++){
-         for(let j = 0; j < horizontal; j++){
-             makeCard( getRandomArbitrary(1,8),i,j);
-         }
-     }
+ } // makeCard(getRandomArbitrary(1, 8), i, j);**/
 
- } 
- arrangeCrds(4,4);
- 
- function makeCard(crdname,axx,axy) {
+function arrangeCrds(vertical, horizontal) {
+    let myarr = [];
+    for(let a = 1; a < (gamerow * gamecol/2) + 1; a++){
+        myarr.push(a);
+        myarr.push(a);
+    }
+    let cardShuffle=[];
+    while(myarr.length > 0){
+        let cardRandom = Math.floor(Math.random()* myarr.length);
+        cardShuffle.push(myarr[cardRandom]);
+        myarr.splice(cardRandom,1);
+
+    }
+
+  for (let i = 0; i < vertical; i++) {
+    for (let j = 0; j < horizontal; j++) {
+      makeCard(cardShuffle.pop(),i, j);
+    }
+  }
+}
+arrangeCrds(gamerow,gamecol);
+
+function makeCard(crdname, axx, axy) {
   let card = document.createElement("img");
-  card.src = "assets/images/crd"+crdname + ".png";
+  card.nAme= crdname;
+  card.src = "assets/images/crdbck.png"; //" + crdname + "
   card.style.position = "absolute";
-  card.style.left= (axy*(cardSizeOne + cardMargin) + cardMargin )+ "px";
-  card.style.top= (axx * (cardSizeOne + cardMargin) + cardMargin )  + "px";
+  card.style.left = axy * (cardSizeOne + cardMargin) + cardMargin + "px";
+  card.style.top = axx * (cardSizeOne + cardMargin) + cardMargin + "px";
   //card.classList.add("cardImage");
+  card.onclick = clickedCard;
   cardGameContainer.appendChild(card);
 }
- /*
+ function clickedCard(event){
+     let card = event.target;
+     card.src = "assets/images/crd"+card.nAme+".png";
+
+     if(firstcard == null){
+         firstcard = card;
+     }else if (secondcard == null){
+        secondcard = card;
+     
+     if(firstcard.nAme == secondcard.nAme){
+         cardGameContainer.removeChild(firstcard);
+         cardGameContainer.removeChild(secondcard);
+     } else{
+         firstcard.src= "assets/images/crdbck.png";
+         secondcard.src= "assets/images/crdbck.png";
+     }
+        firstcard = null;
+        secondcard = null;
+    }
+    
+     
+ }
+/*
  makeCard(1,0,0);
  makeCard(5,0,1);
  makeCard(7,0,2);
@@ -35,10 +74,9 @@
  makeCard(3,1,2);
  makeCard(6,1,3);**/
 
- //makeCard(1,0,3.8);
+//makeCard(1,0,3.8);
 
-
- /*function startTimer(){
+/*function startTimer(){
     let btn = document.getElementById("start-btn");
     btn.addEventListener("click",setInterval(timeSet, 1000));
 }
